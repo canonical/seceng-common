043f7a0a429066bfc0ef3d3b0fd37893335882a9
Ian Constantin <ian.constantin@canonical.com>
2026-02-20 10:14:30 +0200

    Add rsync-daemon subordinate charm

---
+++ rsync-daemon/charm/src/charm.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# Copyright 2026 Canonical Ltd.
+
+import ops
+import pathlib
+import subprocess
+from ops.model import ActiveStatus
+
+from charmlibs.seceng import utils
+from charmlibs.seceng.base import Snap, Package, SecEngCharmBase
+
+
+class RsyncDaemonCharm(SecEngCharmBase):
+    """Charm the service."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+        framework.observe(self.on.config_changed, self._on_config_changed)
+        framework.observe(self.on.secret_changed, self._on_secret_changed)
+        framework.observe(self.on.upgrade_charm, self._on_upgrade_charm)
+
+        framework.observe(self.on.install, self._on_install)
+        framework.observe(self.on.rsync_module_relation_changed, self._on_relation_changed)
+        framework.observe(self.on.rsync_module_relation_broken, self._on_relation_broken)
+
+    def _on_install(self, event: ops.InstallEvent):
+        """ Install rsync and setup the base configuration """
+        self.unit.status = ops.MaintenanceStatus("Installing rsync")
+
+        subprocess.check_call(['apt-get', 'update'])
+        subprocess.check_call(['apt-get', 'install', '-y', 'rsync'])
+
+        # Write the global rsync config
+        rsyncd_config = """uid = nobody
+gid = nogroup
+use chroot = yes
+max connections = 20
+syslog facility = local5
+pid file = /run/rsyncd.pid
+&include /etc/rsyncd.conf.d/
+"""
+        pathlib.Path('/etc/rsyncd.conf').write_text(rsyncd_config)
+        pathlib.Path('/etc/rsyncd.conf.d').mkdir(exist_ok=True)
+
+        subprocess.check_call(['systemctl', 'enable', 'rsync'])
+        subprocess.check_call(['systemctl', 'start', 'rsync'])
+
+        self.unit.status = ops.ActiveStatus("rsync daemon running")
+
+    def _on_relation_changed(self, event: ops.RelationChangedEvent):
+        """ Read data from the principal charm and configure the module """
+        remote_unit = event.unit
+
+        if not remote_unit:
+            return
+
+        # Fetch the data provided by nvd-sync
+        data = event.relation.data[remote_unit]
+        path = data.get('path')
+        module_name = data.get('module_name')
+        read_only = data.get('read_only', 'yes')
+        comment = data.get('comment', '')
+
+        # Ensure the principal charm has populated the data before proceeding
+        if not path or not module_name:
+            return
+
+        self.unit.status = ops.MaintenanceStatus(f"Configuring [{module_name}] module")
+
+        # Write the drop-in configuration
+        conf_content = f"""[{module_name}]
+    path = {path}
+    comment = {comment}
+    read only = {read_only}
+    list = yes
+"""
+        # Using the relation id for the conf filename to allow for proper cleanup when a relation is broken
+        conf_file = pathlib.Path(f'/etc/rsyncd.conf.d/relation-{event.relation.id}.conf')
+        conf_file.write_text(conf_content)
+
+        # Restart to apply changes
+        subprocess.check_call(['systemctl', 'restart', 'rsync'])
+        self.unit.status = ops.ActiveStatus(f"Serving module [{module_name}]")
+
+    def _on_relation_broken(self, event: ops.RelationBrokenEvent):
+        """ Clean up a relation's config when removed """
+        conf_file = pathlib.Path(f'/etc/rsyncd.conf.d/relation-{event.relation.id}.conf')
+
+        if conf_file.exists():
+            conf_file.unlink()
+            subprocess.check_call(['systemctl', 'restart', 'rsync'])
+
+        self.unit.status = ops.ActiveStatus("rsync daemon running")
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
+        self.unit.status = ActiveStatus('ready (config)')
+
+    def _on_secret_changed(self, event: ops.SecretChangedEvent) -> None:
+        self.unit.status = ActiveStatus('ready (secret)')
+
+    def _on_upgrade_charm(self, event: ops.UpgradeCharmEvent) -> None:
+        self.unit.status = ActiveStatus('ready (upgrade)')
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(RsyncDaemonCharm)
